e035e0e4acd1ec068e405adb9fe7020a404adf82
Viharika Chinthapalli <108276259+viharika-Chinthapalli@users.noreply.github.com>
2023-08-01 15:48:07 +0530

    Update Contact.js

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://my-portfolio-api-ten.vercel.app/contact", {
+    let response = await fetch("https://my-portfolio-5z6xxb024-viharika-chinthapalli.vercel.app/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
