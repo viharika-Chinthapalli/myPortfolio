942f8e180f0c6c949ee5c94b90a93f93705711f9
Viharika Chinthapalli <108276259+viharika-Chinthapalli@users.noreply.github.com>
2023-08-08 18:18:25 +0530

    Update Contact.js

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://portfolio-api-ygon.onrender.com/contact", {
+    let response = await fetch("http://localhost:5000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
